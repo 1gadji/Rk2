11ae3c3fcbc773d19dfa55393c091129bc53c3a7
1gadji <sulejmanovgadzi897@gmail.com>
2024-05-20 09:37:56 +0300

    cmake

---
+++ rk2/_build/CMakeFiles/ex3.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ex3.dir/ConceptualExample03.cpp.o"
+  "CMakeFiles/ex3.dir/ConceptualExample03.cpp.o.d"
+  "libex3.a"
+  "libex3.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ex3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ rk2/_build/CMakeFiles/example.dir/Program.cpp.o.d
@@ -0,0 +1,2 @@
+CMakeFiles/example.dir/Program.cpp.o: /home/gadji/rk2/Program.cpp \
+ /usr/include/stdc-predef.h

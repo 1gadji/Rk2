f81f20974a5e121df744e00f9869179a091bfc83
1gadji <sulejmanovgadzi897@gmail.com>
2024-06-04 16:31:57 +0300

    rk2

---
+++ Testy.cpp
@@ -0,0 +1,18 @@
+#include <gtest/gtest.h>
+#include "ConceptualExample01.cpp"
+#include "ConceptualExample02.cpp"
+#include "ConceptualExample03.cpp"
+#include <iostream>
+
+
+
+TEST(IObserver, ConceptualExample01) {
+    ASSERT_NO_THROW(test_conceptual_example_01());
+}
+
+TEST(IObserver, ConceptualExample02) {
+    ASSERT_NO_THROW(test_conceptual_example_02());
+}
+TEST(IObserver, ConceptualExample03) {
+    ASSERT_NO_THROW(test_conceptual_example_03());
+}

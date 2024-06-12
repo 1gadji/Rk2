8f1365dfe5fadd468991efe0658949f12d78b475
1gadji <sulejmanovgadzi897@gmail.com>
2024-06-13 00:35:16 +0300

    first

--- test1/_build/cmake_install.cmake
+++ test1/_build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/adam/test
+# Install script for directory: /home/gadji/test1
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/adam/test/_build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/gadji/test1/_build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
